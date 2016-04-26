82bfbe7797453654b8b4972cbf814319204ad239
Iris Caffin <iriscaffin@gmail.com>
2016-04-25 19:28:24 -0700

    Added user prompts

--- Hello/Hello/Program.cs
+++ Hello/Hello/Program.cs
@@ -10,7 +10,28 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, " + args[0]);
+            // Ask the user for their name, save the name, and respond with Hello + name of user
+
+            Console.WriteLine("Your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name);
+
+
+            // Ask the user if they slept well. If it is less then 8 hours return a phrase and the same for more than 8 hours
+
+            Console.WriteLine("Tell me, how many hours of sleep did you get last night?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            if (hoursOfSleep > 8)
+            {
+                Console.WriteLine("You are well rested, keep this up!");
+            }
+            else
+            {
+                Console.WriteLine("You need more sleep, try to get more tonight!");
+            }
+            
         }
     }
 }
